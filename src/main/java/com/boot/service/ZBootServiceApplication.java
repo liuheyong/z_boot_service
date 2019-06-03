d3e79394ca1fd2195acedb9d5e30678f0e7eb5a2
liuheyong <17682347237@163.com>
2019-06-04 00:09:39 +0800

    创建线程池，使用异步线程

--- src/main/java/com/boot/service/ZBootServiceApplication.java
+++ src/main/java/com/boot/service/ZBootServiceApplication.java
@@ -9,9 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableCaching
+@EnableAsync
 @EnableRedisHttpSession
 @SpringBootApplication
 @EnableDubboConfiguration
